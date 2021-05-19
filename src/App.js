d191e71cbc4878b2674daa03cb1ee2c82b50b5d4
Yug34 <vertilax1337@gmail.com>
2021-05-19 13:07:18 +0530

    Prettified project

--- src/App.js
+++ src/App.js
@@ -1,38 +1,33 @@
 import React, { useEffect, useState } from "react";
-import './App.css';
+import "./App.css";
 import ListProduct from "./pages/ListProducts";
 import CreateProduct from "./pages/CreateProduct";
 
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <Router className="App">
-        <div>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">List</Link>
-                    </li>
-                    <li>
-                        <Link to="/create">Create</Link>
-                    </li>
-                </ul>
-            </nav>
-            <Switch>
-                <Route path="/create">
-                    <CreateProduct/>
-                </Route>
-                <Route path="/">
-                    <ListProduct/>
-                </Route>
-            </Switch>
-        </div>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">List</Link>
+            </li>
+            <li>
+              <Link to="/create">Create</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/create">
+            <CreateProduct />
+          </Route>
+          <Route path="/">
+            <ListProduct />
+          </Route>
+        </Switch>
+      </div>
     </Router>
   );
 }
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,35 +1,44 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
 function ProductCard(props) {
-    let [imgLink, setImgLink] = useState(null)
+  let [imgLink, setImgLink] = useState(null);
 
-    function addToCart() {
-        //    TODO
+  function addToCart() {
+    //    TODO
+  }
+
+  useEffect(() => {
+    function _arrayBufferToBase64(buffer) {
+      let binary = "";
+      let bytes = new Uint8Array(buffer);
+      let len = bytes.byteLength;
+      for (let i = 0; i < len; i++) {
+        binary += String.fromCharCode(bytes[i]);
+      }
+      return window.btoa(binary);
     }
 
-    useEffect(() => {
-        function _arrayBufferToBase64(buffer) {
-            let binary = "";
-            let bytes = new Uint8Array(buffer);
-            let len = bytes.byteLength;
-            for (let i = 0; i < len; i++) {
-                binary += String.fromCharCode(bytes[i]);
-            }
-            return window.btoa(binary);
-        }
-        setImgLink(`data:${props.image.contentType};base64,${_arrayBufferToBase64(props.image.data.data)}`)
-    }, [props]);
-
-    return (
-        <div className="App">
-            <div>{props.name}</div>
-            <div>{props.description}</div>
-            <div>{props.price}</div>
-            <div>{props.quantity}</div>
-            <img src={imgLink} alt={props.name} style={{width: "150px", height: "150px"}}/>
-            <button onClick={addToCart}>Add to cart</button>
-        </div>
+    setImgLink(
+      `data:${props.image.contentType};base64,${_arrayBufferToBase64(
+        props.image.data.data
+      )}`
     );
+  }, [props]);
+
+  return (
+    <div className="App">
+      <div>{props.name}</div>
+      <div>{props.description}</div>
+      <div>{props.price}</div>
+      <div>{props.quantity}</div>
+      <img
+        src={imgLink}
+        alt={props.name}
+        style={{ width: "150px", height: "150px" }}
+      />
+      <button onClick={addToCart}>Add to cart</button>
+    </div>
+  );
 }
 
 export default ProductCard;
